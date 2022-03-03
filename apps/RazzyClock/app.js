20df8f1558f4e12adb01be7b6d69ffc162a656de
razzykitty <74110725+razzykitty@users.noreply.github.com>
2022-03-02 20:46:46 -0700

    Update app.js

--- apps/RazzyClock/app.js
+++ apps/RazzyClock/app.js
@@ -10,22 +10,45 @@
   let timeSize = 50; //Size of font for time
   let dateSize = 15; //Size of font for date
 
-  let Steps   = 15;
+  var steps; //Used for rainbow
+
+  g.setTheme({bg:"#FFFFFF"});
+  g.setTheme({fg:"#000000"});
+
+  let colorBG = g.theme.bg; //Gets BG color
+  let colorFG = g.theme.fg; //Gets FG color
+
+  var is12Hour = (require("Storage").readJSON("setting.json", 1) || {})["12hour"]; //What's the clock set to?
 
 function draw() {
   
-  //draws rainbow backgrounf
-  let StepHeight = (ScreenHeight-25)/Steps;
+  var d = new Date(); //What's the date?
+  var hours = d.getHours(); // what hour is it?
+  
 
-  for (let i = 0; i < Steps; i++) {
-      let Color = E.HSBtoRGB(i/Steps,1,1, true);
+  //draws rainbow background
+  
+  steps = hours; //Dynamically sets rainbow steps to time
+  if(steps<7) steps = 7; // If there are less than 7 steps, force 7
+  
+  let StepHeight = (ScreenHeight-25)/steps;
+
+  for (let i = 0; i < steps; i++) {
+      let Color = E.HSBtoRGB(i/steps,1,1, true);
       g.setColor(Color[0]/255,Color[1]/255,Color[2]/255);
       g.fillRect(0,(i*StepHeight)+25, 175,(i+1)*(StepHeight)+25);
     }
+  
   // work out how to display the current time
-  var d = new Date();
   var da = d.toString().split(" ");
-  var time = da[4].substr(0, 5);
+
+  if (is12Hour) hours = ((hours + 11) % 12) + 1;
+
+  var time = hours + ":" + ("0" + d.getMinutes()).substr(-2);
+  //var time = da[4].substr(0, 5);
+  
+
+  //var time = da[4].substr(0, 5);
   var date = [da[0], da[1], da[2], da[3]].join(" ");
   
   var timeCenterX = centerX;
@@ -33,14 +56,14 @@ function draw() {
   // Reset the state of the graphics library
   g.reset();
   g.setFont("Vector",timeSize);
-  // draw the current time (4x size 7 segment)
+  // draw the current time with a border
   g.setFontAlign(0,0);
-  g.setColor("#FFFFFF");
+  g.setColor(colorBG);
   g.drawString(time,timeCenterX-timeBorder,timeCenterY).drawString(time,timeCenterX+timeBorder,timeCenterY);
   g.drawString(time,timeCenterX,timeCenterY-timeBorder).drawString(time,timeCenterX,timeCenterY+timeBorder);
   g.drawString(time,timeCenterX-timeBorder,timeCenterY-timeBorder).drawString(time,timeCenterX-timeBorder,timeCenterY+timeBorder);
   g.drawString(time,timeCenterX+timeBorder,timeCenterY-timeBorder).drawString(time,timeCenterX+timeBorder,timeCenterY+timeBorder);
-  g.setColor("#000000");
+  g.setColor(colorFG);
   g.drawString(time, timeCenterX, timeCenterY, false);
   
 
@@ -50,18 +73,18 @@ function draw() {
   g.setFont("Vector",dateSize);
   // draw the current time (4x size 7 segment)
   g.setFontAlign(0,0);
-  g.setColor("#FFFFFF");
+  g.setColor(colorBG);
   g.drawString(date,dateCenterX-dateBorder,dateCenterY).drawString(date,dateCenterX+dateBorder,dateCenterY);
   g.drawString(date,dateCenterX,dateCenterY-dateBorder).drawString(date,dateCenterX,dateCenterY+dateBorder);
   g.drawString(date,dateCenterX-dateBorder,dateCenterY-dateBorder).drawString(date,dateCenterX-dateBorder,dateCenterY+dateBorder);
   g.drawString(date,dateCenterX+dateBorder,dateCenterY-dateBorder).drawString(date,dateCenterX+dateBorder,dateCenterY+dateBorder);
-  g.setColor("#000000");
+  g.setColor(colorFG);
   g.drawString(date, dateCenterX, dateCenterY, false);
 }
 
   g.clear();
   draw();
-  var secondInterval = setInterval(draw, 1000);
+  var secondInterval = setInterval(draw, 60000);
 
   Bangle.setUI("clock");
 
